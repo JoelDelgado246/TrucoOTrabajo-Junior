c773bfb0643a8a3be75684d8fc4a1ba25dbb33ac
Joel Guingla <luis.guingla@epn.edu.ec>
2024-10-26 07:12:17 -0500

    Refactor routing in App.jsx: Remove duplicate route path

--- packages/Frontend/src/App.jsx
+++ packages/Frontend/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/comunidad" element={<Comunidad />} />
-        <Route path="/" element={<Home />} />
         <Route path="/trucos" element={<Trucos />} />
         <Route path="/trucos/:id" element={<ChallengeDetail />} />
         <Route path="/tratos" element={<Tratos />} />
